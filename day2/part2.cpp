900641e571a7a784211d5e8a35b989424e504866
Ryan Mast <nightlark@users.noreply.github.com>
2017-12-01 22:00:43 -0800

    C++ solution for day 2 part 2

---
+++ day2/part2.cpp
@@ -0,0 +1,78 @@
+#include <string>
+#include <iostream>
+
+int main() {
+	  std::string input[] = {
+"5  9 2 8",
+"9  4 7 3",
+"3  8 6 5"
+};
+
+    // Sum of evenly divisible values dividing each other
+    int sum = 0;
+    
+    for (auto line : input)
+    {
+        int val1 = 0;
+        int val2 = 0;
+        
+        // The start position in the line for the first value
+        std::string::size_type idx1 = 0;
+        
+        while (idx1 < line.length())
+        {
+            // The end position in the line for the first value
+            auto val1_pos_end = line.find("\t", idx1+1);
+            val1_pos_end = (val1_pos_end == -1) ? line.length() : val1_pos_end;
+            
+            // The substring containing only the first value
+            auto val1_str = line.substr(idx1, val1_pos_end);
+            
+            // String to int for the value, with the number of characters used
+            std::string::size_type sz = 0;
+            int val1 = std::stoi(val1_str, &sz);
+            idx1 += sz;
+            
+            // The start position in the line for the second value
+            std::string::size_type idx2 = 0;
+            while (idx2 < line.length())
+            {
+                // The end position in the line for the second value
+                auto val2_pos_end = line.find("\t", idx2+1);
+                val2_pos_end = (val2_pos_end == -1) ? line.length() : val2_pos_end;
+            
+                // The substring containing only the second value
+                auto val2_str = line.substr(idx2, val2_pos_end);
+            
+                // String to int for the value, with the number of characters used
+                int val2 = std::stoi(val2_str, &sz);
+                idx2 += sz;
+                
+                // Don't test the value against itself
+                if (idx1 != idx2)
+                {
+                    // Check if the numbers are evenly divisible
+                    if (val1 % val2 == 0 || val2 % val1 == 0)
+                    {
+                        if (val1 > val2)
+                        {
+                            sum += val1/val2;
+                        }
+                        else
+                        {
+                            sum += val2/val1;
+                        }
+                        
+                        // Kill the loops early and skip to next line
+                        idx1 = line.length();
+                        idx2 = line.length();
+                    }
+                }
+            }
+            
+        }
+    }
+    
+    // Print the result
+    printf("%d\n", sum);
+}
