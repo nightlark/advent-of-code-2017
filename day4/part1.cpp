441389526e8bfe6149d571df0106f40fcd1ddf77
Ryan Mast <nightlark@users.noreply.github.com>
2017-12-06 19:07:54 -0800

    Solution to day 4 part 1 [C++]

---
+++ day4/part1.cpp
@@ -0,0 +1,49 @@
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <set>
+
+// Function to check if the passphrase is valid
+bool validPassphrase (std::string passphrase) {
+    // Track previously seen words
+    std::set<std::string> prev_words;
+    
+    std::istringstream ss (passphrase);
+    std::string word;
+    
+    while (std::getline(ss, word, ' '))
+    {
+        // Fail if we've already seen a word
+        if (prev_words.find(word) != prev_words.end())
+        {
+            return false;
+        }
+        
+        // Add word to the list of previously seen words    
+        prev_words.insert(word);
+    }
+    
+    // Valid if no word was seen twice
+    return true;
+}
+
+int main() {
+    // The number of valid passphrases
+    int num_valid_passphrases = 0;
+    
+    // Reads passphrases and checks if they are valid
+    try {
+        std::string passphrase;
+        while (std::getline(std::cin, passphrase))
+        {
+            if (validPassphrase (passphrase))
+            {
+                num_valid_passphrases++;
+            }
+        }
+    } catch (...) {}
+    
+    // Print the number of valid passphrases
+    printf("%d\n", num_valid_passphrases);
+}
